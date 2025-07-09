2b227cf5c1c56ce216310d2409f9a73321f7b4fb
gugelhupf50 <gugelhupf50@gmail.com>
2025-07-09 13:04:53 -0500

    Add split MOHELA_DataUnlock archive

---
+++ rejoin_mohela.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Rejoining MOHELA_DataUnlock.7z files...
+copy /b MOHELA_DataUnlock.7z.aa+MOHELA_DataUnlock.7z.ab+MOHELA_DataUnlock.7z.ac+MOHELA_DataUnlock.7z.ad+MOHELA_DataUnlock.7z.ae+MOHELA_DataUnlock.7z.af+MOHELA_DataUnlock.7z.ag+MOHELA_DataUnlock.7z.ah+MOHELA_DataUnlock.7z.ai+MOHELA_DataUnlock.7z.aj+MOHELA_DataUnlock.7z.ak+MOHELA_DataUnlock.7z.al+MOHELA_DataUnlock.7z.am+MOHELA_DataUnlock.7z.an+MOHELA_DataUnlock.7z.ao MOHELA_DataUnlock.7z
+echo Done! The file MOHELA_DataUnlock.7z has been recreated.
+pause
